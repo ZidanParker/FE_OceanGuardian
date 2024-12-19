9d2a012b788cf23c5bbdb09fcdb7116ce2aadd76
Zakaria Zidan Zamroni <161435839+ZidanParker@users.noreply.github.com>
2024-12-19 10:57:57 +0700

    update/fixLogo

--- src/Component/SideBarAdmin.jsx
+++ src/Component/SideBarAdmin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 import dashboardIcon from '../assets/dashboard.png';
 import dokumentasiIcon from '../assets/dokumentasi.png';
 import helpIcon from '../assets/help.png';
