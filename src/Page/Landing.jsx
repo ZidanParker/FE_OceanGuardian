fa0b8ff66d50e60c096ad71bcafa041bfb936c48
Zakaria Zidan Zamroni <161435839+ZidanParker@users.noreply.github.com>
2024-12-19 11:06:06 +0700

    update/fixImagage

--- src/Page/Landing.jsx
+++ src/Page/Landing.jsx
@@ -29,7 +29,7 @@ const Landing = () => {
           </div>
           <div className="md:w-1/2 flex justify-center items-center">
             <img
-              src="src/assets/terumbu.png"
+              src="assets/terumbu.png"
               alt="Coral Illustration"
               className="w-full max-w-xs md:max-w-lg"
             />
@@ -40,10 +40,10 @@ const Landing = () => {
           <h2 className="text-black text-2xl md:text-3xl font-bold text-center mb-16 md:mb-28">FITUR-FITUR</h2>
           <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 px-4">
             {[
-              { src: "src/assets/laporan.png", title: "Laporan Real-Time", description: "Fitur ini menyediakan informasi dari laporan pengguna lain secara real-time." },
-              { src: "src/assets/ikan.png", title: "Pelaporan Penangkapan Ikan Ilegal", description: "Fitur ini memungkinkan pengguna untuk melaporkan adanya penangkapan ikan ilegal yang mereka temukan." },
-              { src: "src/assets/sampah.png", title: "Pelaporan Sampah Laut", description: "Fitur ini memungkinkan pengguna untuk melaporkan adanya sampah laut yang mereka temukan." },
-              { src: "src/assets/statistik.png", title: "Statistik Pelaporan", description: "Fitur ini memungkinkan pengguna melihat statistik pelaporan per tahun maupun per bulan." },
+              { src: "assets/laporan.png", title: "Laporan Real-Time", description: "Fitur ini menyediakan informasi dari laporan pengguna lain secara real-time." },
+              { src: "assets/ikan.png", title: "Pelaporan Penangkapan Ikan Ilegal", description: "Fitur ini memungkinkan pengguna untuk melaporkan adanya penangkapan ikan ilegal yang mereka temukan." },
+              { src: "assets/sampah.png", title: "Pelaporan Sampah Laut", description: "Fitur ini memungkinkan pengguna untuk melaporkan adanya sampah laut yang mereka temukan." },
+              { src: "assets/statistik.png", title: "Statistik Pelaporan", description: "Fitur ini memungkinkan pengguna melihat statistik pelaporan per tahun maupun per bulan." },
             ].map((feature, index) => (
               <div key={index} className="bg-blue-500 text-white rounded-lg shadow-md p-6 text-center flex flex-col">
                 <img src={feature.src} alt={feature.title} className="mx-auto mb-4 w-full max-w-xs h-auto object-cover rounded-lg" />
